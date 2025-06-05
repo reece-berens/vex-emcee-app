e4f462ff840660df92b0e5d5e1ca866a40297c71
benjaminferreira <benjamin.j.ferreira@gmail.com>
2025-06-04 23:04:07 -0500

    Initial layout added - started on filters

---
+++ next-app/app/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="layout-header">
+            <h3>Welcome to VEX Emcee - select an event using search boxes below</h3>
+        </header>
+    );
+}
--- next-app/app/page.js
+++ next-app/app/page.js
@@ -1,7 +1,28 @@
 export default function Home() {
     return (
-        <div>
-            <h1>HomePage</h1>
-        </div>
+        <main className="layout-main">
+            <div className="layout-content">
+                <div id="search-filters-id" className="layout-content__filters">
+                    <select>
+                        <option value="">Select Program (V5, IQ, U, etc.)</option>
+                        <option value="program1">Program 1</option>
+                        <option value="program2">Program 2</option>
+                        <option value="program3">Program 3</option>
+                    </select>
+                    <select>
+                        <option value="">Select Region</option>
+                        <option value="region1">Region 1</option>
+                        <option value="region2">Region 2</option>
+                        <option value="region3">Region 3</option>
+                    </select>
+                    <select>
+                        <option value="">Select Event Name</option>
+                        <option value="event1">Event 1</option>
+                        <option value="event2">Event 2</option>
+                        <option value="event3">Event 3</option>
+                    </select>
+                </div>
+            </div>
+        </main>
     );
 }
