b5e47698b087d76230b822e7868d69dcf17d7189
Ben Ferreira <benjamin.j.ferreira@gmail.com>
2025-06-21 21:50:48 -0500

    Next app 2 (more next things) (#7)
    * Updating visuals (again) / adding needed nextjs packages also
    
    * Making changes to account for smallest mobile device width
    
    * Misc next app package traces? Not sure about this one tbh
    
    * Fixing layout to flex grow properly. Messing with media properties for smaller screens.
    
    * Removing .next folder from branch - updating gitignore

--- next-app/app/page.js
+++ next-app/app/page.js
@@ -3,30 +3,39 @@ import styles from "./Home.module.css";
 export default function Home() {
     return (
         <main className="layout-main">
+            <div className="layout-sidebar">
+                <h3>Welcome to VEX Emcee!</h3>
+                <p>This is a sidebar for larger screens.</p>
+            </div>
             <div className="layout-content">
-                <div id="search-filters-id" className={styles.filters}>
-                    <select>
+                <h2>Select an event using the filters below:</h2>
+                <div id="search-filters-id" className={`${styles.filters} cardback`}>
+                    <select id="program-select">
                         <option value="">Select Program (V5, IQ, U, etc.)</option>
                         <option value="program1">Program 1</option>
                         <option value="program2">Program 2</option>
                         <option value="program3">Program 3</option>
                     </select>
-                    <select>
+                    <select id="region-select">
                         <option value="">Select Region</option>
                         <option value="region1">Region 1</option>
                         <option value="region2">Region 2</option>
                         <option value="region3">Region 3</option>
                     </select>
-                    <select>
+                    <select id="event-select">
                         <option value="">Select Event Name</option>
                         <option value="event1">Event 1</option>
                         <option value="event2">Event 2</option>
                         <option value="event3">Event 3</option>
                     </select>
+                    <div className={styles.buttonWrapper}>
+                        <button className="btn-primary">Search</button>
+                    </div>
                 </div>
-                <div className={styles.buttonWrapper}>
-                    <button className="btn-primary">Search</button>
-                </div>
+            </div>
+            <div className="layout-extra">
+                <h3>Welcome to VEX Emcee!</h3>
+                <p>This is a sidebar for larger screens.</p>
             </div>
         </main>
     );
